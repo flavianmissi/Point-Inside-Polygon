d2e0cd2b09b6584bad7766426b7829a2cdc4066e
Flavia Missi <flaviamissi@gmail.com>
2011-03-08 16:37:54 -0300

    Removed unused methods

--- polygon.py
+++ polygon.py
@@ -2,13 +2,6 @@ class Polygon(object):
     def __init__(self, coordinates):
         self.coordinates = coordinates
 
-    def get_vertical_sides(self):
-        vertical_coordinates = []
-        for i in range(1, len(self.coordinates)):
-            if self.coordinates[i][0] == self.coordinates[i-1][0]:
-                vertical_coordinates.append([[self.coordinates[i-1][0], self.coordinates[i-1][1]], [self.coordinates[i][0], self.coordinates[i][1]]])
-        return vertical_coordinates
-
     def get_number_of_intersected_sides(self, test_point):
         crossed_by_left = 0; crossed_by_right = 0
         print self.coordinates
--- test_point_inside_polygon.py
+++ test_point_inside_polygon.py
@@ -3,17 +3,6 @@ from nose.tools import *
 from polygon import Polygon
 
 class PointInsidePolygonTestCase(unittest.TestCase):
-    def test_should_retrieve_1_5_5_4_4_1_vertical_sides(self):
-        A = [2, 1]; B = [2, 5]; C = [6, 5]; D = [6, 4]; E = [4, 4]; F = [4, 1]
-        polygon = Polygon([A, B, C, D, E, F])
-        assert_equals(polygon.get_vertical_sides(), [[[2, 1], [2, 5]], [[6, 5], [6, 4]], [[4, 4], [4, 1]]])
-
-    def test_should_retrieve_6_9_11_9_9_6_vertical_sides(self):
-        A = [1, 2]; B = [1, 4]; C = [3, 4]; D = [3, 6]; E = [5, 6];
-        F = [5, 4]; G = [7, 4]; H = [7, 6]; I = [9, 6]; J = [9, 2]
-        polygon = Polygon([A, B, C, D, E, F, G, H, I, J])
-        assert_equals(polygon.get_vertical_sides(), [[[1, 2], [1,4]], [[3, 4], [3, 6]], [[5, 6], [5, 4]], [[7, 4], [7, 6]], [[9, 6], [9, 2]]])
-
     def test_should_retrive_each_side_that_crosses_the_Y_from_the_test_point(self):
         A = [3, 6]; B = [3, 11]; C = [6, 11]; D = [6, 9]; E = [5, 9]; F = [5, 6]
         polygon = Polygon([A, B, C, D, E, F])
